205a62488ef09e02754083df5a99889d6d88ce55
marthagram <mrsmartha.otas@gmail.com>
2025-11-26 20:22:08 +0100

    form project building

---
+++ scripts/form.js
@@ -0,0 +1,56 @@
+const year = document.querySelector("#currentyear");
+
+const lastModified = document.getElementById("lastModified")
+
+
+const today = new Date();
+
+year.innerHTML = `&copy ${today.getFullYear()}  🌼 Martha Itohan Otasowie 🌼 Nigeria`;
+lastModified.innerHTML = ` Last Modification: ${ document.lastModified}`;
+
+
+
+const products = [
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
+];
+
+//  Populate the Product Name options where the array's name field is used for the select option display and the array's id is used for the value field.
+
+const select = document.getElementById("select-product")
+
+products.forEach((product) => {
+    
+    select.innerHTML += `
+                    <option value="${product.id}">${product.name}</option>`
+});
+
+// adding an event listener method to the select element can also work:
+
+// select.addEventListener(click, ()=>{  })               
+
+
+
